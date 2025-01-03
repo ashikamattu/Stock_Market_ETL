46c819437a9c8a1d3efe320e888136283429e835
ashikamattu <ashikamatt1@gmail.com>
2025-01-02 21:44:41 -0500

    * write stocks_info_df to csv file in data folder, with timestamp in file name. *Set up google cloud CLI and upload the csv file to relevant GCS bucket under the intended GCP project *make notes in experiment.ipynb while trying out gcloud cli

--- code/extract_to_gcs.py
+++ code/extract_to_gcs.py
@@ -1,7 +1,7 @@
 import requests
 import csv
 import pandas as pd
-#from google.cloud import storage
+from google.cloud import storage
 
 tickers_df = pd.read_csv('./tickers.csv', dtype={'Symbol': str})
 tickers_df['Symbol'] = tickers_df['Symbol'].str.strip()
@@ -12,45 +12,52 @@ headers = {
 	"x-rapidapi-key": "6fe0cabaefmsh7ef3e2799f96313p13bba4jsn8315bbcfaddb",
 	"x-rapidapi-host": "yahoo-finance166.p.rapidapi.com"
 }
-i = 0
 
 field_names = ['ticker','currentPrice','totalRevenue','ebitda','freeCashflow','profitMargins','revenueGrowth','debtToEquity','totalDebt','numberOfAnalystOpinions','recommendationKey','timeStamp']
 stocks_info_df = pd.DataFrame(columns = field_names)
 
 for ticker in tickers_df['Symbol']:
-    # For dev purpose only
-    if (i == 3):
-        break
     querystring = {"region":"US","symbol":ticker}
     response = requests.get(url, headers=headers, params=querystring)
 
     if response.status_code == 200:
-        data = response.json().get('quoteSummary', []).get('result',[])
+        data = response.json().get('quoteSummary', []).get('result',[])#.get('0',[]).get('financialData',[])
         stock_info_dict = {}
         stock_info_dict['ticker'] = ticker
-        # stock_info_dict['timestamp'] = timestamp 
+        current_timestamp = datetime.datetime.now() 
+        stock_info_dict['timeStamp'] = current_timestamp 
         for entry in data[0]['financialData']:
-            if ((entry in stocks_info_df.columns) and entry != 'recommendationKey'):
+            if ((entry in stocks_info_df.columns) and entry != 'recommendationKey' and data[0]['financialData'][entry] != {}):
                 stock_info_dict[entry] = data[0]['financialData'][entry]['raw']
             if ((entry in stocks_info_df.columns) and entry == 'recommendationKey'):
                 stock_info_dict[entry] = data[0]['financialData'][entry]
         stocks_info_df = pd.concat([stocks_info_df,pd.DataFrame([stock_info_dict])], ignore_index = True)
-
     else:
         print("Failed to fetch data:", response.status_code)
 
-    i += 1
 # Validate and Upload stocks_info_df to CSV file
+current_timestamp = stocks_info_df['timeStamp'].max()
+csv_filename = str('./data/daily_stock_data_'+str(current_timestamp).replace(":", "-")+'.csv')
+
+stocks_info_df.to_csv(csv_filename, index=False)
 
 # Upload the CSV file to GCS
-'''
+
 bucket_name = 'bkt-stocks'
-#storage_client = storage.Client()
-#bucket = storage_client.bucket(bucket_name)
-destination_blob_name = f'{csv_filename}'  # The path to store in GCS
+source_file_name = csv_filename 
+destination_blob_name = csv_filename  
 
+storage_client = storage.Client(project='noted-point-444318-r3')
+bucket = storage_client.bucket(bucket_name)
 blob = bucket.blob(destination_blob_name)
-blob.upload_from_filename(csv_filename)
+blob.upload_from_filename(source_file_name)
 
-print(f"File {csv_filename} uploaded to GCS bucket {bucket_name} as {destination_blob_name}")
+# TODO - Implement Logging system to log the success of the upload
+print(f"File {source_file_name} uploaded to {destination_blob_name} in bucket {bucket_name}.")
+
 '''
+TODO: Check if the file you are uploading already exists in the bucket. Or GCP might handle this already. Double check. 
+blobs = storage_client.list_blobs(bucket_name)
+
+for blob in blobs:
+    print(blob.name)'''
